3a0665123353b18682217e86ec9392fdeefdf029
MEUNIER Sébastien <stg3005@brufor.lan>
2024-04-08 15:47:57 +0200

    input move suite

--- Assets/#Project/Scripts/DirectActionInput.cs
+++ Assets/#Project/Scripts/DirectActionInput.cs
@@ -4,22 +4,27 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 public class DirectActionInput : MonoBehaviour
 {
-    public InputAction moveHorizontal;
+    //public InputAction moveHorizontal;
+    public InputAction move;
     public InputAction shoot;
     public float speed;
 
     public void OnEnable(){
-        moveHorizontal.Enable();
+        //moveHorizontal.Enable();
+        move.Enable();
         shoot.Enable();
     }
     public void OnDisable(){
-        moveHorizontal.Disable();
+        //moveHorizontal.Disable();
+        move.Disable();
         shoot.Disable();
     }
     void Update(){
-        float movement = moveHorizontal.ReadValue<float>();
-        movement = Mathf.Clamp(movement, -1, 1);
-        transform.Translate(speed* Time.deltaTime * movement, 0, 0);
+        //float movement = moveHorizontal.ReadValue<float>();
+        //movement = Mathf.Clamp(movement, -1, 1);
+        //transform.Translate(speed* Time.deltaTime * movement, 0, 0);
+        Vector2 movement = move.ReadValue<Vector2>();
+        transform.Translate(speed* Time.deltaTime * movement.x, 0, speed* Time.deltaTime * movement.y);
     }
     public void Awake(){
         shoot.performed += ctx => {OnShoot(ctx);};
